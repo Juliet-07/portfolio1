c08ba242557320b6012bf0ba5bd8a23c8403b9f3
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-09-15 11:37:38 +0100

    portfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-
 import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Next from "../assets/next.png";
 import ReactNative from "../assets/reactnative.png"
-import Node from "../assets/node.png";
-import FireBase from "../assets/firebase.png";
-import AWS from "../assets/aws.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
-import Mongo from "../assets/mongo.png";
+// import Node from "../assets/node.png";
+// import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
